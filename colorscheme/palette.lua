2db2ad1a1ca3580717bc2249d3c07f61d0eef672
dangminhngo <dangminhngo.dev@gmail.com>
2024-01-27 10:12:09 +0700

    fix: update themes

--- colorscheme/palette.lua
+++ colorscheme/palette.lua
@@ -2,46 +2,47 @@ local color = require("colorscheme.color")
 local helpers = require("colorscheme.lib.helpers")
 
 local builtin = {
-	alpha = {
-		fg = "#c5d5df",
-		bg = "#0d1418",
-		red = "#e46769",
-		green = "#a4c76f",
-		yellow = "#eace60",
-		blue = "#619af5",
-		magenta = "#9d78d1",
-		cyan = "#42b8e6",
-		teal = "#5fd7aa",
-		orange = "#e39d5f",
-		pink = "#d983d7",
-	},
-	gamma = {
-		fg = "#cfcabe",
-		bg = "#141617",
+	forest = {
+		fg = "#cfc7c2",
+		bg = "#242b2f",
 		red = "#e67e80",
-		green = "#a4c76f",
-		yellow = "#d1cc66",
-		blue = "#678dc9",
-		magenta = "#a183cc",
-		cyan = "#76bfc4",
-		teal = "#6fc7a4",
-		orange = "#d69760",
-		pink = "#c975b9",
+		green = "#a7c080",
+		yellow = "#dbbc7f",
+		blue = "#6c86db",
+		magenta = "#b495d3",
+		cyan = "#76b4cc",
+		teal = "#76cca1",
+		orange = "#e69875",
+		pink = "#d699b6",
+	},
+	midnight = {
+		fg = "#c4bab4",
+		bg = "#21272b",
+		red = "#e67e80",
+		green = "#a7c080",
+		yellow = "#dbbc7f",
+		blue = "#6c86db",
+		magenta = "#b495d3",
+		cyan = "#76b4cc",
+		teal = "#76cca1",
+		orange = "#e69875",
+		pink = "#d699b6",
 	},
 }
 
 return function(style, accent)
 	style = string.lower(style)
 	accent = string.lower(accent)
-	local p = builtin[style or "alpha"]
+	local p = builtin[style or "forest"]
+	print(p)
 
 	p = helpers.tbl_extend(p, {
 		name = "Crux",
 		dark = color.rel_darken(p.bg, 0.32),
 		bg0 = color.rel_darken(p.bg, 0.16),
-		bg2 = color.rel_lighten(p.bg, 0.02),
-		bg3 = color.rel_lighten(p.bg, 0.06),
-		bg4 = color.rel_lighten(p.bg, 0.1),
+		bg2 = color.rel_lighten(p.bg, 0.04),
+		bg3 = color.rel_lighten(p.bg, 0.08),
+		bg4 = color.rel_lighten(p.bg, 0.12),
 		fg0 = color.rel_lighten(p.fg, 0.64),
 		fg2 = color.rel_darken(p.fg, 0.16),
 		fg3 = color.rel_darken(p.fg, 0.32),
